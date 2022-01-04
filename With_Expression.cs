d982c8effcfc05e358079a8f7118ab3a19a60e70
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-01-04 14:30:52 +0530

    Add files via upload

---
+++ With_Expression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InitOnly
+{
+    public record Company
+    {
+        public int ID { get; set; } 
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company obj1 = new Company { ID = 1, Name = "ABC", City = "Mumbai" };
+            Console.WriteLine(obj1.ID.ToString() + " " + obj1.Name + " " + obj1.City);
+
+            Company obj2 = obj1 with {  Name="XYZ", City = "Thane" };
+            Console.WriteLine(obj2.ID.ToString()+" "+ obj2.Name+" "+ obj2.City);
+
+            Console.ReadLine();
+        }
+    }
+}
